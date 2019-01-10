414636b07a201c0c77366955ebbcab1acfe56eb0
scyren <dream-h-go@hotmail.com>
2019-01-10 12:49:21 +0100

    add promise, dico internaute, module.export, clean code

---
+++ dico/fr_internaute.js
@@ -0,0 +1,68 @@
+// Internaute
+module.exports.recursiveMap = function (defin, splitter, defmap) {
+
+    if (defin.indexOf("Traduction anglais") > 0) {
+        defin = defin.split(splitter)
+            .join("|")
+            .split("|");
+        defmap[splitter] = defin[1].trim();
+        this.recursiveMap(defin[0], "Synonyme", defmap);
+    } else if (defin.indexOf("Synonyme") > 0 || defin.indexOf("Synonymes") > 0) {
+        defin = defin
+            .split("Synonymes :")
+            .join("|")
+            .split("Synonyme :")
+            .join("|")
+            .split("|");
+        defmap["Synonyme"] = defin[1].trim();
+        this.recursiveMap(defin[0], "Exemple :", defmap);
+    } else if (defin.indexOf("Exemple :") > 0) {
+        defin = defin.split("Exemple :").join("|").split("|");
+        defmap["Exemple"] = defin[1].trim();
+        this.recursiveMap(defin[0], "NONE", defmap);
+    } else {
+        defmap["Definition"] = defin.trim();
+    }
+}
+
+
+// Get Definition
+module.exports.getDefinition = function ($, definitions) {
+
+    $(".dico_definition .dico_liste li .grid_last").each(function (i, elem) {
+
+        let definition_map = {};
+        let defin = [];
+        defin[i] = $(this).text();
+
+        module.exports.recursiveMap(defin[i], "Traduction anglais", definition_map);
+
+        definitions["Sens " + (i + 1)] = {
+            'Theme': $(this).prev('.grid_left').children('.dico_exergue_1').text(),
+            ...definition_map
+        };
+
+    });
+
+}
+
+// Get Synonymes 
+module.exports.getSynonymes = function ($, synonymes) {
+    $(".dico_synonymes .dico_liste li").each(function (i, elem) {
+        synonymes[i] = $(this).text().trim();
+    });
+}
+
+// Get Citations 
+module.exports.getCitations = function ($, citations) {
+    $(".dico_citations .dico_liste li").each(function (i, elem) {
+
+        let citation_map = {};
+
+        citation_map.citation = $(this).children().children().first().text().trim(),
+        citation_map.author = $(this).children().children().last().text().trim(),
+
+        citations[i] = { ...citation_map };
+        
+    });
+}
--- index.js
+++ index.js
@@ -1,117 +1,101 @@
 #!/usr/bin/env node
 
+
 const request = require('request');
 const cheerio = require('cheerio');
 
-
 var program = require('commander');
 
+var internaute = require("./dico/fr_internaute.js")
+
+// console.log("what : " + internaute.recursiveMap());
+//  return false;
 program
 	.version('0.1.0')
 	.option('-l, --lang [lang]', 'Choose language [fr,en,fr-en,en-fr]')
 	.option('-w, --word [word]', 'Add a word [word]')
-  .option('-s, --syn', 'Synonymes')
-  .option('-c, --cit', 'Citation')
-  .option('-d, --def', 'Definition')
+	.option('-s, --syn', 'Synonymes')
+	.option('-c, --cit', 'Citation')
+	.option('-d, --def', 'Definition')
 	.parse(process.argv);
 
 
 if (program.word) {
-  console.log('Your word is : ' + program.word);
-  word = program.word;
+	console.log('Your word is : ' + program.word);
+	word = program.word;
 } else {
-  console.log('you must input a word --help');
-  process.exit();
+	console.log('you must input a word --help');
+	process.exit();
 }
 
-
-request(
-	"https://www.linternaute.fr/dictionnaire/fr/definition/" + word,
-  (err, res, body) => {
-
-    //Load HTML body into cheerio
-    const $ = cheerio.load(body, { normalizeWhitespace: false });
-
-    var  definition = synonymes =[];
-    let sensMap = {};
-    let citations = {};
-
-    function recursiveMap(defin, splitter, defmap){
-      if (defin.indexOf("Traduction anglais") > 0) {
-          defin = defin
-            .split(splitter)
-            .join("|")
-            .split("|");
-			  defmap[splitter] = defin[1].trim();
-          recursiveMap(defin[0], "Synonyme", defmap);
-        } else if (defin.indexOf("Synonyme") > 0 || defin.indexOf("Synonymes") > 0) {
-          defin = defin
-            .split("Synonymes :")
-            .join("|")
-            .split("Synonyme :")
-            .join("|")
-            .split("|");
-			  defmap["Synonyme"] = defin[1].trim();
-            recursiveMap(defin[0], "Exemple :", defmap);
-	  	}else if (defin.indexOf("Exemple :") > 0) {
-            defin = defin.split("Exemple :").join("|").split("|");
-            defmap["Exemple"] = defin[1].trim();
-            recursiveMap(defin[0], "NONE", defmap);
-        } else {
-			defmap["Definition"] = defin.trim();
-        }
-    }
-
-    // Get Definitions
-    $(".dico_definition .dico_liste li .grid_last").each(function(i, elem) {
-          
-		  let defMap = {};
-      definition[i] = $(this).text();
-
-      recursiveMap(definition[i], "Traduction anglais", defMap);
-      
-      sensMap["Sens " + (i + 1)] = { 'Theme': $(this).prev('.grid_left').children('.dico_exergue_1').text(), ...defMap };
-    
-	  });
-  
-    // Get synonymes
-    $(".dico_synonymes .dico_liste li").each(function (i, elem) {
-      synonymes[i] = $(this).text().trim();
-    });
-    
-    // Get Citations
-    $(".dico_citations .dico_liste li").each(function (i, elem) {
-     
-      let citMap = {};
-
-      citMap['citation'] = $(this).children().children().first().text().trim(),
-      citMap['author'] = $(this).children().children().last().text().trim(),
-
-      citations[i] = { ...citMap }
-      
-    });
+var definitions = {};
+var synonymes = [];
+var citations = {};
 
 
-	  sensMap["title_def"] = $('section.dico_main_title span.dico_title_definition').text().trim()
-	  
-	  
-    defaultDef = 'def';
-    
-    if (program.syn) {
-      console.log(synonymes);
-      defaultDef = '';
-    }
-    
-    if (program.cit) {
-      console.log(citations);
-      defaultDef = '';
-    }
+function initialize() {
+	// Setting URL and headers for request
+	var options = {
+		url: 'https://www.linternaute.fr/dictionnaire/fr/definition/' + word,
+	};
+	// Return new promise 
+	return new Promise(function (resolve, reject) {
 
-    if (program.def || defaultDef=='def') {
-      console.log(JSON.stringify(sensMap, null, " ").trim());
-    }
-    
-    // console.log(JSON.stringify(sensMap, null, " ").trim());
 
-  }
-);
+		// Do async job
+		request.get(options, function (err, resp, body) {
+			if (err) {
+				reject(err);
+			} else {
+
+				const $ = cheerio.load(body, {
+					normalizeWhitespace: false
+				});
+
+				// Definition
+				internaute.getDefinition($, definitions);
+
+				// Get synonymes
+				internaute.getSynonymes($, synonymes);
+
+				// Get Citations
+				internaute.getCitations($, citations);
+
+				result = {
+					"definitions": definitions,
+					"synonymes": synonymes,
+					"citations": citations,
+				}
+				resolve(result);
+			}
+		})
+	})
+}
+
+function main() {
+	var initializePromise = initialize();
+	initializePromise.then(function (result) {
+		// userDetails = result.syn;
+		console.log("Initialized ");
+
+		defaultDef = 'def';
+		if (program.syn) {
+			console.log(result.synonymes);
+			defaultDef = '';
+		}
+
+		if (program.cit) {
+			console.log(result.citations);
+			defaultDef = '';
+		}
+
+		if (program.def || defaultDef == 'def') {
+			console.log(JSON.stringify(result.definitions, null, " ").trim());
+		}
+
+	}, function (err) {
+		console.log(err);
+	})
+}
+
+main();
